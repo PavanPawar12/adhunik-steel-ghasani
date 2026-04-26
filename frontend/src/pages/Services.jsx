d4ba0a11f7704b98f5e8e83493c29ca80e84afe9
PavanPawar12 <pavanpawar9923100@gmail.com>
2026-04-26 16:14:50 +0530

    commit changes

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,30 +1,4 @@
-// import image1 from "../assets/adhunic_assets/image1.jpeg";
-// import image2 from "../assets/adhunic_assets/image2.jpeg";
-// import image3 from "../assets/adhunic_assets/image3.jpeg";
-// import image4 from "../assets/adhunic_assets/image4.jpeg";
-// import image5 from "../assets/adhunic_assets/image5.jpeg";
-
-// import ServiceCard from "../components/ServiceCard.jsx";
-
-// const Services = () => {
-//   return (
-//     <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-6">
-      
-//       <ServiceCard image={image1} title="all card" />
-//       <ServiceCard image={image2} title="card1" />
-//       <ServiceCard image={image3} title="card2" />
-//       <ServiceCard image={image4} title="card2" />
-//       <ServiceCard image={image5} title="card2" />
-      
-//     </div>
-//   );
-// };
-
-// export default Services;
-
-
 import { useState } from "react";
-
 import image1 from "../assets/adhunic_assets/image1.jpeg";
 import image2 from "../assets/adhunic_assets/image2.jpeg";
 import image3 from "../assets/adhunic_assets/image3.jpeg";
